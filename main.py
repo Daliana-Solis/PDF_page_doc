5e9aa4b1312909d18d479046eece477eb8c73896
Daliana-Solis <dsolisso@uci.edu>
2023-09-14 18:19:22 -0500

    Added footers for each page

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 #p = portrait, l = landscape
 pdf = FPDF(orientation="P", unit="mm", format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -28,16 +29,21 @@ for index, row in df.iterrows():
     #add line (add x1, y1, x2,y2) start to end coordinates
     pdf.line(10, 21, 200, 21)
 
+    #Set footer
+    #add 270 breaklines
+    pdf.ln(260)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=10, txt=row['Topic'],align='R', ln=1)
+
+
     pages = int(row['Pages'])-1
     for _ in range(pages):
         pdf.add_page()
-
-
-
-
-
-
-
+        pdf.ln(272)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(100, 100, 100)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R', ln=1)
 
 pdf.cell(w=0, h=12, txt='Hello', align='L', ln=1, border=1)
 
